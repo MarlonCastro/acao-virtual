7a215a7bb376c8af0e0b4965d61c22f7873e6bc9
MarlonCastro <marloncastro78@gmail.com>
2017-12-02 01:14:21 -0200

    Update and rename src/environments/firebase.config.ts to  src/ ambientes/firebase.config.ts

--- src/environments/firebase.config.ts
+++
@@ -1,8 +0,0 @@
-export const FirebaseConfig = {
-    apiKey: "AIzaSyDtKkTCfzLwz3VyIMjIusCN8rkunLX7808",
-    authDomain: "acaovirtual-e88f3.firebaseapp.com",
-    databaseURL: "https://acaovirtual-e88f3.firebaseio.com",
-    projectId: "acaovirtual-e88f3",
-    storageBucket: "acaovirtual-e88f3.appspot.com",
-    messagingSenderId: "555475381679"
-  };
